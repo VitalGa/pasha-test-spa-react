d623d861d366aaa509319af18d530f5c9eab08b7
VitalGa <vitalga10@gmail.com>
2024-03-20 15:43:22 +0400

    Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,30 @@
+
 import { useState, useEffect } from 'react';
 import styles from './Card.module.scss';
 import axios from 'axios';
 import Placeholder from '../Placeholder/Placeholder';
 
-const Card = () => {
+const Card = ({ selectedValue }) => {
   const [starships, setStarships] = useState([]);
+  
+  console.log(selectedValue)
 
   useEffect(() => {
     axios.get('https://swapi.dev/api/starships/')
       .then(response => {
-        const sortedStarships = response.data.results.sort((a, b) => a.name.localeCompare(b.name));
+        let sortedStarships = [];
+        if (selectedValue === 'Name' || selectedValue === 'name') {
+          sortedStarships = response.data.results.sort((a, b) => a.name.localeCompare(b.name));
+        } else if (selectedValue === 'Cost' || selectedValue === 'cost') {
+          sortedStarships = response.data.results.sort((a, b) => a.cost_in_credits - b.cost_in_credits);
+        }
+        
         setStarships(sortedStarships);
       })
       .catch(error => {
         console.error(error);
       });
-  }, []);
+  }, [selectedValue]);
 
   return (
     <div>
@@ -35,7 +44,7 @@ const Card = () => {
             </div>
             <div className={styles['name']}>
               <span>Name:</span>
-              <span>{starship.name.length > 20 ? `${starship.name.substring(0, 20)}...` : starship.name}</span>
+              <span>{starship.name.length > 20 ? `${starship.name.substring(0, 18)}...` : starship.name}</span>
             </div>
           </div>
         </div>
--- src/components/Starships/Starships.jsx
+++ src/components/Starships/Starships.jsx
@@ -1,26 +1,53 @@
-import { useState } from 'react'
-import Select from '../Select/Select'
-import styles from './Starships.module.scss'
-import Card from '../Card/Card'
+// import { useState } from 'react'
+// import Select from '../Select/Select'
+// import styles from './Starships.module.scss'
+// import Card from '../Card/Card'
+
+// const Starships = () => {
+
+//   const [selectedValue, setSelectedValue] = useState('Name')
+
+//   const handleSelectChange = (value) => {
+//     setSelectedValue(value)
+//   }
+
+//   return (
+//     <>
+//       <div className={styles['title']}>Starships</div>
+//         <div className={styles['menu']}>
+//           <div className={styles['text']}>Sort by:</div>
+//           <Select selectedValue={selectedValue} onSelectChange={handleSelectChange}/>
+//         </div>
+//       <Card/>
+//     </>
+//   )
+// }
+
+// export default Starships
+
+import { useState } from 'react';
+import Select from '../Select/Select';
+import styles from './Starships.module.scss';
+import Card from '../Card/Card';
 
 const Starships = () => {
-
-  const [selectedValue, setSelectedValue] = useState('Name')
+  const [selectedValue, setSelectedValue] = useState('Name');
 
   const handleSelectChange = (value) => {
-    setSelectedValue(value)
-  }
+    setSelectedValue(value);
+  };
 
   return (
     <>
       <div className={styles['title']}>Starships</div>
-        <div className={styles['menu']}>
-          <div className={styles['text']}>Sort by:</div>
-          <Select selectedValue={selectedValue} onSelectChange={handleSelectChange}/>
-        </div>
-      <Card/>
+      <div className={styles['menu']}>
+        <div className={styles['text']}>Sort by:</div>
+        <Select selectedValue={selectedValue} onSelectChange={handleSelectChange} />
+      </div>
+      <Card selectedValue={selectedValue} />
     </>
-  )
-}
+  );
+};
 
-export default Starships
+export default Starships;
+
