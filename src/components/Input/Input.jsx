7ec20a8752f72c8edd1a25c9be3644378a693f96
VitalGa <vitalga10@gmail.com>
2024-04-01 13:04:35 +0400

    add zod

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,52 +1,51 @@
-import styles from './Input.module.scss';
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import styles from './Input.module.scss';
+
+const schema = z.object({
+  firstName: z.string()
+    .min(8, { message: 'Min length must be 8 characters' })
+    .max(15, { message: 'Max length must be 15 characters' }),
+  lastName: z.string()
+    .min(8, { message: 'Min length must be 8 characters' })
+    .max(15, { message: 'Max length must be 15 characters' })
+});
 
 const Input = () => {
-
   const {
-    register, 
-    handleSubmit, 
-    formState: { errors } 
-  } = useForm();
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: zodResolver(schema)
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
-    <form className={styles.wrapper} onSubmit={handleSubmit((data) => {
-      console.log(data);
-    })}>
-      <span>First Name:</span>
-      <input 
-        className={errors.firstName ? styles.error : ''}
-        {...register('firstName', {
-          required: 'This is required',
-          minLength: {
-            value: 8,
-            message: 'Min length must be 8 characters'
-          },
-          maxLength: {
-            value: 15,
-            message: 'Max length must be 15 characters'
-          }
-        })}
-      />
-      {errors.firstName && <p className={styles.errorText}>{errors.firstName.message}</p>}
-      <span>Last Name:</span>
-      <input 
-        className={errors.lastName ? styles.error : ''}
-        {...register('lastName', {
-          required: 'This is required',
-          minLength: {
-            value: 8,
-            message: 'Min length must be 8 characters'
-          },
-          maxLength: {
-            value: 15,
-            message: 'Max length must be 15 characters'
-          }
-        })}
-      />
-      {errors.lastName && <p className={styles.errorText}>{errors.lastName.message}</p>}
-      <input className={styles.button} type='submit'/> 
-    </form>
+    <div className={styles['container']}>
+      <form className={styles['wrapper']} onSubmit={handleSubmit(onSubmit)}>
+        <span>First Name:</span>
+        <input 
+          className={`${styles.firstName} ${errors.firstName ? styles.error : ''}`}
+          {...register('firstName')}
+        />
+        {errors.firstName && <p className={styles.errorText}>{errors.firstName.message}</p>}
+        
+        <span>Last Name:</span>
+        <input 
+          className={`${styles.lastName} ${errors.firstName ? styles.error : ''}`}
+          {...register('lastName')}
+        />
+        {errors.lastName && <p className={styles.errorText}>{errors.lastName.message}</p>}
+        
+        <input className={styles.button} type='submit'/> 
+      </form>
+    </div>
+    
   );
 }
 
