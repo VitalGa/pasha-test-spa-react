11c0dd58fecca8aa09e4cbc51d50cbc01ad9bbfc
VitalGa <vitalga10@gmail.com>
2024-04-16 00:03:06 +0400

    add Root State and Dispatch Types

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,12 @@
+// import styles from './Form.module.scss'
+import Input from '../Input/Input'
+
+
+const Form = () => {
+
+  return (
+    <Input />
+  )
+}
+
+export default Form
---
+++ src/store/menu.slice.tsx
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { boolean } from 'zod';
+
+const initialState = {
+  menuIcon: false
+};
+
+export const menuSlice = createSlice({
+  name: 'menu',
+  initialState,
+  reducers: {
+    // Экшен для переключения состояния `menuIcon`
+    toggleMenuIcon: (state, action) => {
+      state.menuIcon = action.payload;
+    },
+    // Экшен для установки состояния `menuIcon` в false
+    closeMenuIcon: (state, action) => {
+      state.menuIcon = action.payload;
+    },
+  },
+});
+
+export const { toggleMenuIcon, closeMenuIcon } = menuSlice.actions;
+
+export default menuSlice.reducer;
